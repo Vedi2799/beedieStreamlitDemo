2e33d78b8c71dc1e192a75b72d4e4a42d605e3cc
Vedi2799 <125392985+Vedi2799@users.noreply.github.com>
2023-02-14 13:45:38 -0800

    hihi

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ st.write("APP WORKING!")
 tickers = ['AAPL','MSFT','NFLX','IBM','DIS']
 ticker = st.selectbox("Pick a ticker", tickers) 
 
-df pd.read_csv(ticker+".csv", parse_dates=['Date'], index_col=['Date'])
+df = pd.read_csv(ticker+".csv", parse_dates=['Date'], index_col=['Date'])
 
 st.write(df)
