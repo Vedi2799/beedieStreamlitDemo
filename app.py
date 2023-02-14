7d8f57b866b76026a8228fbfc17479899c74c8a5
Vedi2799 <125392985+Vedi2799@users.noreply.github.com>
2023-02-14 13:44:20 -0800

    made a bunch of changes

--- app.py
+++ app.py
@@ -4,4 +4,8 @@ import streamlit as st
 st.write("APP WORKING!")
 
 tickers = ['AAPL','MSFT','NFLX']
-st.selectbox("Pick a ticker", tickers) 
+ticker = st.selectbox("Pick a ticker", tickers) 
+
+df pd.read_csv(ticker+".csv", parse_dates=['Date'], index_col=['Date'])
+
+st.write(df)
